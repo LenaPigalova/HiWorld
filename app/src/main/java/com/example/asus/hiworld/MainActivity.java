24997f38f4a75b7096a45b0797606ca043e0b424
LenaPigalova <lena-06-07yandex.ru>
2019-01-21 15:52:04 +0500

    this commit add methods of life cicle and logs. It change text colors.

--- app/src/main/java/com/example/asus/hiworld/MainActivity.java
+++ app/src/main/java/com/example/asus/hiworld/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.asus.hiworld;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("CREATE", "method onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("START", "method onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("RESUME", "method onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("PAUSE", "method onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("STOP", "method onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("DESTROY", "method onDestroy");
     }
 }
